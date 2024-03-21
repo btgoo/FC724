3f093ec32fde214f53971e8c4ab0630b533c26a0
btgoo <btgonbayr@gmail.com>
2024-03-21 23:04:09 +0000

    Second update: putting flask into action, connecting the html file with flask file.

--- Assignment.py
+++ Assignment.py
@@ -2,3 +2,12 @@ from flask_wtf import Form
 from flask import Flask, render_template
 from wtforms import StringField, PasswordField
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'blank'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('Example.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
